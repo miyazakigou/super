35a9d6af64d0ed3fe1e54ac9a6f0bd5c4b28f82b
安河内 理希 <236115@kcs-learning.local>
2025-11-25 10:11:18 +0900

    利用規約画面

---
+++ Supersaurus/food_map/lib/pages/Terms_of_Service.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class TermsOfServicePage extends StatelessWidget {
+  const TermsOfServicePage({super.key});
+
+  Widget build(BuildContext context) {
+    body: Center(
+      children: [
+        const Text("利用規約画面", style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
+        
+      ]
+    )
+  }
+}
