c1de6cf7ced7224b717b7eb7e86de1938b9a95fb
宮崎 豪 <236143@kcs-learning.local>
2025-12-03 12:12:07 +0900

    うううううう

--- Supersaurus/food_map/android/app/build.gradle.kts
+++ Supersaurus/food_map/android/app/build.gradle.kts
@@ -2,11 +2,13 @@ plugins {
     id("com.android.application")
     id("kotlin-android")
     id("dev.flutter.flutter-gradle-plugin")
+    id("com.google.gms.google-services")
 }
 
 android {
     namespace = "com.example.food_map"
     compileSdk = 36
+
     defaultConfig {
         applicationId = "com.example.food_map"
         minSdk = flutter.minSdkVersion
@@ -14,28 +16,37 @@ android {
         versionCode = 1
         versionName = "1.0"
     }
+
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
     }
+
     kotlinOptions {
         jvmTarget = "11"
     }
+
     signingConfigs {
-        release {
-            storeFile file("my-release-key.keystore")
-            storePassword "123456"
-            keyAlias "my-key-alias"
-            keyPassword "123456"
-        }
-    buildTypes {
-        release {
-            signingConfig signingConfigs.release
+        create("release") {
+            storeFile = file("my-release-key.keystore")
+            storePassword = "123456"
+            keyAlias = "my-key-alias"
+            keyPassword = "123456"
         }
     }
-}
 
-apply(plugin = "com.google.gms.google-services")
+    buildTypes {
+    release {
+        signingConfig = signingConfigs.getByName("release")
+        isMinifyEnabled = true
+        isShrinkResources = true
+    }
+    debug {
+        isMinifyEnabled = false
+        isShrinkResources = false
+    }
+}
+}
 
 flutter {
     source = "../.."
